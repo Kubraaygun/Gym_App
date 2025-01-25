58fc442652c071e147c522bff7ffd9b903ffefa6
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2025-01-26 01:28:59 +0300

    Update BodyPart

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,7 +1,17 @@
 import React from "react";
 import { Stack, Typography } from "@mui/material";
+import Icon from "../assets/icons/gym.png";
+import { Height } from "@mui/icons-material";
 const BodyPart = () => {
-  return <div>BodyPart</div>;
+  return (
+    <Stack>
+      <img
+        src={Icon}
+        alt="dumbell"
+        styles={{ width: "40px", height: "40px" }}
+      />
+    </Stack>
+  );
 };
 
 export default BodyPart;
