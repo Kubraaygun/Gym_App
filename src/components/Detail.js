1f4b9395cac3a66ae513725362b0066b4ba8968e
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2025-01-27 03:16:01 +0300

    updated ExerciseDetail

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -5,8 +5,16 @@ import BodyPart from "./BodyPart";
 import BodyPartImage from "../assets/icons/body-part.png";
 import TargetImage from "../assets/icons/target.png";
 import EquipmentImage from "../assets/icons/equipment.png";
-const Detail = () => {
-  return <div>Detail</div>;
+const Detail = ({ exerciseDetail }) => {
+  const { BodyPart, gifUrl, name, target, equipment } = exerciseDetail;
+  return (
+    <Stack
+      gap="60px"
+      sx={{ flexDirection: { lg: "row" }, p: "20px", alignItems: "center" }}
+    >
+      <img src={gifUrl} alt={name} loading="lazy" className="detail-image" />
+    </Stack>
+  );
 };
 
 export default Detail;
