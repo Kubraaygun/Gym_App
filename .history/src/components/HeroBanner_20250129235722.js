c7646e678e81dc0f21d22c3805a6935345085a40
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2025-01-30 00:03:28 +0300

    Updated Readme

---
+++ .history/src/components/HeroBanner_20250129235722.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { Box, Typography, Button, useMediaQuery } from "@mui/material";
+import HeroBannerImage from "../assets/images/banner.jpg";
+
+const HeroBanner = () => {
+  const isSmallScreen = useMediaQuery("(max-width:1200px)");
+  const isLargeScreen = useMediaQuery("(min-width:2000px)");
+
+  return (
+    <Box
+      sx={{
+        mt: { lg: "212px", xs: "70px" },
+        p: "20px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: isSmallScreen || isLargeScreen ? "center" : "flex-start",
+        textAlign: isSmallScreen || isLargeScreen ? "center" : "left",
+        ml: isSmallScreen || isLargeScreen ? "0px" : { sm: "50px" },
+      }}
+    >
+      <Typography color="#FF2625" fontWeight="600" fontSize="40">
+        Fitness Salonu
+      </Typography>
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "40px" } }}
+        mb="23px"
+        mt="30px"
+        color={"#3A1212"}
+        fontFamily="Alegreya"
+      >
+        Gülümse ve <br /> Tekrar Et
+      </Typography>
+      <Typography
+        fontSize="22px"
+        lineHeight="35px"
+        mb={3}
+        fontFamily="Alegreya"
+      >
+        En etkili egzersizleri inceleyin
+      </Typography>
+      <Button
+        variant="contained"
+        color="error"
+        href="#exercises"
+        sx={{ backgroundColor: "#ff2625", padding: "10px" }}
+      >
+        Egzersiz keşfedin
+      </Button>
+      <Typography
+        fontWeight={600}
+        color="#ff2625"
+        sx={{ opacity: 0.1, display: { lg: "block", xs: "none" } }}
+        fontSize="200px"
+      >
+        Egzersiz
+      </Typography>
+      <img
+        src={HeroBannerImage}
+        alt="banner"
+        className="hero-banner-img"
+        style={{
+          borderBottomLeftRadius: "80px",
+          height: "97%",
+          width: "100%",
+          maxWidth: "600px",
+        }}
+      />
+    </Box>
+  );
+};
+
+export default HeroBanner;
---
+++ .history/src/components/SearchExercises_20250129235546.js
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from "react";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+import HorizontalScrollBar from "./HorizontalScrollBar";
+
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
+  const [search, setSearch] = useState("");
+  const [bodyParts, setBodyParts] = useState([]);
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+        exerciseOptions
+      );
+      setBodyParts(["all", ...bodyPartsData]);
+    };
+    fetchExercisesData();
+  }, []);
+
+  const handleSearch = async () => {
+    if (search) {
+      const exercisesData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises?limit=1000",
+        exerciseOptions
+      );
+      const searchedExercises = exercisesData.filter(
+        (item) =>
+          item.name.toLowerCase().includes(search) ||
+          item.target.toLowerCase().includes(search) ||
+          item.equipment.toLowerCase().includes(search) ||
+          item.bodyPart.toLowerCase().includes(search)
+      );
+      window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
+      setSearch("");
+      setExercises(searchedExercises);
+    }
+  };
+  return (
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "40px" } }}
+        mb="50px"
+        textAlign="center"
+        color={"#3A1212"}
+      >
+        Bilmeniz Gereken <br /> Harika Egzersizler
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          sx={{
+            input: { fontWeight: "700", border: "none", borderRadius: "4px" },
+            width: { lg: "800px", xs: "350px" },
+            backgroundColor: "#fff",
+            borderRadius: "40px",
+          }}
+          value={search}
+          height="76px"
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+          placeholder="Egzersiz Ara"
+          type="text"
+        />
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: "#FF2625",
+            color: "#fff",
+            textTransform: "none",
+            width: { lg: "173px", xs: "80px" },
+            height: "56px",
+            position: "absolute",
+            right: "0px",
+            fontSize: { lg: "20px", xs: "14px" },
+          }}
+          onClick={handleSearch}
+        >
+          Ara
+        </Button>
+      </Box>
+
+      <Box sx={{ position: "relative", width: "100%", p: "20px" }}>
+        <HorizontalScrollBar
+          data={bodyParts}
+          isBodyParts
+          setBodyPart={setBodyPart}
+          bodyPart={bodyPart}
+        />
+      </Box>
+    </Stack>
+  );
+};
+
+export default SearchExercises;
